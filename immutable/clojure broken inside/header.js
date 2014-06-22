14c860628ee5d1d530f5105eae772c3156d6041a
spolsh <michal.m.klos@gmail.com>
2014-06-22 22:26:49 +0200

    but in clojures we can break things inside

---
+++ immutable/clojure broken inside/header.js
@@ -0,0 +1,23 @@
+var createSth = ( function () {
+	var FIVE = 5; // it will be seen to all functions, but not outside
+	console.log( 'sth created' );
+	return {
+		doStuff: function () {
+			return 'doStuff:' + FIVE++; // but we can break it inside
+		},
+		
+		addStuff: function ( val ) {
+			// FIVE sits still there,
+			// value is not inserted only at execution time of clojure
+			return 'addStuff:' + ( FIVE + val );
+		}
+	}
+});
+
+var sth = createSth();
+console.log( 'header:', sth.doStuff(), sth.addStuff( 0 ), sth.FIVE, sth );
+
+window.onload = onLoad;
+function onLoad() {
+	console.log( 'onLoad:', sth.doStuff(), sth.addStuff( 0 ), sth.FIVE, sth );
+};
