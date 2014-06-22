2154f4736fef71803b9a3aafee3ad4d907f3bde4
spolsh <michal.m.klos@gmail.com>
2014-06-22 22:52:45 +0200

    freezing clojured object

---
+++ immutable/clojure immutable inside/header.js
@@ -0,0 +1,31 @@
+var createSth = ( function () {
+	// var FIVE = 5;
+	// Object.freeze( FIVE ); // lets freeze it! throws Object.freeze called on non-object 
+	
+	var constants = {
+		FIVE: 5
+	};
+	Object.freeze( constants ); // lets freeze it!
+	
+	console.log( 'sth created' );
+	
+	return {
+		doStuff: function () {
+			return 'doStuff:' + constants.FIVE++; // but we can break it inside
+		},
+		
+		addStuff: function ( val ) {
+			// FIVE sits still there,
+			// value is not inserted only at execution time of clojure
+			return 'addStuff:' + ( constants.FIVE + val );
+		}
+	}
+});
+
+var sth = createSth();
+console.log( 'header:', sth.doStuff(), sth.addStuff( 1 ), sth.constants, sth );
+
+window.onload = onLoad;
+function onLoad() {
+	console.log( 'onLoad:', sth.doStuff(), sth.addStuff( 1 ), sth.constants, sth );
+};
