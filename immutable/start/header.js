951191956626f4a9b00472459c1cc9c2411c952e
spolsh <michal.m.klos@gmail.com>
2014-06-22 21:36:20 +0200

    Revert "broken example"
    This reverts commit 0f95ddf2f58b691954bf473c42f70ab47f000228.

--- immutable/start/header.js
+++ immutable/start/header.js
@@ -1,14 +1,7 @@
-var constants = ( function () {
-	var _FIVE = 5; // will be assigned on each call
-	return {
-		FIVE: _FIVE
-	}
-});
-
-console.log( 'header:', constants(), constants().FIVE );
+console.log( 'header' );
 
 window.onload = onLoad;
 
 function onLoad() {
-	console.log( 'onLoad:', constants(), constants().FIVE );
+	console.log( 'onLoad' )
 };
