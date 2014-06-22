b90ddc88d33ba18d7b7e4bb088153f581e5aa16d
spolsh <michal.m.klos@gmail.com>
2014-06-22 21:41:26 +0200

    broken function statement

--- immutable/function_statement/header.js
+++ immutable/function_statement/header.js
@@ -1,8 +1,8 @@
-var HIGH_FIVE = 5;
-console.log( 'header:', HIGH_FIVE );
+function HIGH_FIVE () { return 5; };
+console.log( 'header:', HIGH_FIVE(), HIGH_FIVE );
 
 window.onload = onLoad;
 
 function onLoad() {
-	console.log( 'onLoad:', HIGH_FIVE )
+	console.log( 'onLoad:', HIGH_FIVE ); //  HIGH_FIVE() - number not a function error
 };
